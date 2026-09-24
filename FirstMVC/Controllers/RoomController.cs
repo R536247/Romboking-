b5b1b0dad9abe94235eb5e2d7e8617d5ceedc5aa
HelDraugar <91600617+HelDraugar@users.noreply.github.com>
2026-09-24 08:44:45 +0200

    Added test data as List.

--- FirstMVC/Controllers/RoomController.cs
+++ FirstMVC/Controllers/RoomController.cs
@@ -1,13 +1,34 @@
 using Microsoft.AspNetCore.Mvc;
-using FirstMVC.Models;
+using StudyRoomBooking.Models;
 
-namespace FirstMVC.Controllers;
+namespace StudyRoomBooking.Controllers;
 
 public class RoomController : Controller
 {
+    // added test/prototype data
+    private List<Room> _rooms = new()
+    {
+        new Room
+        {
+            RoomId = 1,
+            Name = "Study Room A",
+            Building = "Pilestredet 35",
+            Capacity = 6,
+            HasScreen = true
+        },
+
+        new Room
+        {
+                        RoomId = 2,
+            Name = "Study Room B",
+            Building = "Pilestredet 52",
+            Capacity = 4,
+            HasScreen = false
+        }
+    };
     public RoomController()
     {
-        
+
     }
 
     // Pending Views/Rooms/Index.cshtml
