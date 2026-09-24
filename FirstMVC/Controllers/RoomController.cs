98726c49d473084527d28a5a5c3fe5fa158f9634
HelDraugar <91600617+HelDraugar@users.noreply.github.com>
2026-09-24 08:24:28 +0200

    Started on RoomController

---
+++ FirstMVC/Controllers/RoomController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using FirstMVC.Models;
+
+namespace FirstMVC.Controllers;
+
+public class RoomController : Controller
+{
+    public RoomController()
+    {
+        
+    }
+
+    // Pending Views/Rooms/Index.cshtml
+    public IActionResult Index()
+    {
+        
+    }
+}
